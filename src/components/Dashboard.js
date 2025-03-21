a8669bc10a210011daf1d7b4c9f66e6e1766e401
Stuti19 <stutib19@gmail.com>
2025-03-21 21:10:10 +0530

    Initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <h2>Coming Soon!</h2>
+      <p>Your dashboard content will appear here.</p>
+    </div>
+  );
+};
+
+export default Dashboard; 
